be1aae9225a80ca509fbae34a3fb418b59b9b848
sharshenaliev <59019588+sharshenaliev@users.noreply.github.com>
2024-04-06 01:51:55 +0600

    Update urls

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('api/', include('backend.urls')),
+    path('', include('backend.urls')),
 ]
 
 
